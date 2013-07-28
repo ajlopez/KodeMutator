02ca596c016a27edd48ae1a130a8ef9c27c35a36
ajlopez <webmaster@ajlopez.com>
2013-07-28 18:51:49 -0300

    Many mutations

--- test/mutate.js
+++ test/mutate.js
@@ -22,3 +22,39 @@ assert.ok(result)
 assert.notEqual(result, text);
 assert.equal(result.length, text.length);
 assert.ok(result.indexOf('-') > 0);
+assert.equal(occurs(result, '-'), 1);
+
+// many mutations
+
+var counts = [];
+
+for (var k = 0; k < 100; k++) {
+    var result = kodemutator.mutate(text, { replace: { '+': '-' } });
+
+    assert.ok(result)
+    assert.notEqual(result, text);
+    assert.equal(result.length, text.length);
+    assert.equal(occurs(result, '-'), 1);
+    
+    var position = result.indexOf('-');
+    
+    if (counts[position])
+        counts[position]++;
+    else
+        counts[position] = 1;     
+}
+
+assert.ok(counts[2] > 0);
+assert.ok(counts[6] > 0);
+assert.ok(counts[10] > 0);
+
+function occurs(text, character) {
+    var count = 0;
+
+    for (var k = 0; k < text.length; k++)
+        if (text[k] === character)
+            count++;
+            
+    return count;
+}
+
