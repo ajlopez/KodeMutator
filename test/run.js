729c07a04e1ea19f1ab7ac990d5bd5de30e8c7b4
ajlopez <webmaster@ajlopez.com>
2013-07-28 19:54:54 -0300

    Mutate file

--- test/run.js
+++ test/run.js
@@ -25,5 +25,52 @@ function testCleanup() {
             console.dir(err);
         assert.ok(!err);
         assert.ok(!fs.existsSync('./test/temp1'));
+        testRun();
     });
 }
+
+// run run
+
+function testRun() {
+    kodemutator.run({ 
+        setup: 'cmd /c mkdir .\\test\\temp1',
+        run: 'cmd /c rmdir .\\test\\temp1',
+        }, function (err, result) {
+        if (err)
+            console.dir(err);
+        assert.ok(!err);
+        assert.ok(!fs.existsSync('./test/temp1'));
+        testFile();
+    });
+}
+
+// run run
+
+function testFile() {
+    if (fs.existsSync('./test/temp')) {
+        if (fs.existsSync('./test/temp/text.txt'));
+            fs.unlinkSync('./test/temp/text.txt');
+            
+        fs.rmdirSync('./test/temp');
+    }
+        
+    fs.mkdirSync('./test/temp');
+    
+    kodemutator.run({ 
+        setup: 'xcopy .\\test\\files\\text.txt .\\test\\temp',
+        file: '.\\test\\temp\\text.txt',
+        options: {
+            replace: { '+': '-' }
+        }
+        }, function (err, result) {
+        if (err)
+            console.dir(err);
+        assert.ok(!err);
+        assert.ok(fs.existsSync('./test/temp/text.txt'));
+        var text = fs.readFileSync('./test/temp/text.txt').toString();
+        assert.ok(text.indexOf('-') > 0);
+        fs.unlinkSync('./test/temp/text.txt');
+        fs.rmdirSync('./test/temp');
+    });
+}
+
